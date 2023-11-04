5eb20d194b23e6605a44a6ce7298c7c044c0dc5a
dy <85780390+dheerajyadav7571@users.noreply.github.com>
2023-11-04 10:03:30 +0000

    hello world

---
+++ 01helloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+namespace helloWorld
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("hello World , it's gonna be learning paradise ");
+        } 
+
+    }
+}
